8625d7b815ad4f28411dceaaadf48fe77713a7c2
Jack Hinshelwood <jack.hinshelwood@kcl.ac.uk>
2018-01-09 21:01:04 +0000

    Add more chords

--- Chords.java
+++ Chords.java
@@ -55,8 +55,19 @@ public class Chords {
 	}
 
 	public void setupChords() {
+		//Major7 Chords		
 		addChord(new Chord("CMaj7", "C", "E", "G", "B", "D"));
 		addChord(new Chord("DMaj7", "D", "F#", "A", "C#", "E"));
 		addChord(new Chord("EMaj7", "E", "G#", "B", "D#", "F#"));
+		addChord(new Chord("FMaj7", "F", "A", "C", "E", "G"));
+		addChord(new Chord("GMaj7", "G", "B", "D", "F", "A"));
+		addChord(new Chord("AMaj7", "A", "C#", "E", "G#", "B"));
+		addChord(new Chord("BMaj7", "B", "D#", "F#", "A#", "C#"));
+		addChord(new Chord("EbMaj7", "Eb", "G", "Bb", "D", "F"));
+		addChord(new Chord("DbMaj7", "Db", "F", "Ab", "C", "Eb"));
+		addChord(new Chord("AbMaj7", "Ab", "C", "Eb", "G", "Bb"));
+		addChord(new Chord("BbMaj7", "Bb", "D", "F", "A", "C"));
+		
+		
 	}
 }
